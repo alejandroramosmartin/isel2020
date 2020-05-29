bf13e75d3e5da41a1555b15992ee93a72bc1f1a4
alejandroramosmartin <alexramartin@gmail.com>
2020-05-29 18:39:29 +0200

    ej. 12 version 1 - funciona el control de luz

--- 12/src/domotic.c
+++ 12/src/domotic.c
@@ -1,36 +1,113 @@
 #include "domotic.h"
 
-int LightButtonPressed (fsm_t* this) {
-	int button_light = 0;
+int button_light = 0;
+int light_active = 0;
+int light_timeout = 0;
 
-	// button_light = 
 
-	return button_light;
-}
-
-int LightTimeoutReached (fsm_t* this) {
+int LightButtonPressed (fsm_t *this) {
 	int result = 0;
+
+	result = (button_light & LIGHT_ACTIVE);
 
 	return result;
 }
 
-void ActivateLight (fsm_t* this) {
+int LightTimeoutReached (fsm_t *this) {
+	int timeout = 0;
 
-}
+	unsigned int now = clock()/CLOCKS_PER_SEC;
 
-void DeactivateLight (fsm_t* this) {
-
-}
-
-void delay_until (unsigned int next) {
-	unsigned int now =  millis();
-
-	if (next > now) {
-		delay (next-now);
+	if (now >= light_timeout) {
+		timeout = 1;
 	}
+
+	return timeout;
 }
+
+void ActivateLight (fsm_t *this) {
+
+	button_light &= (~LIGHT_ACTIVE);
+
+	// Start/restart the timer
+	unsigned int now = clock()/CLOCKS_PER_SEC;
+	light_timeout = now + 60;	//The light switches off after 60 seconds
+
+	light_active = 1;
+
+	printf("Domotic Controller State: [LIGHT ON]\n");
+	fflush(stdout);
+}
+
+void DeactivateLight (fsm_t *this) {
+
+	light_active = 0;
+
+	printf("Domotic Controller State: [LIGHT OFF]\n");
+	fflush(stdout);
+}
+
+// Introduces a delay until now >= next in milliseconds
+void delay_until (unsigned int next) {
+	unsigned int now =  clock()/CLOCKS_PER_SEC * 1000;	//Current time since the process started (in ms)
+
+	while(next > now){
+		now = clock()/CLOCKS_PER_SEC * 1000;
+	}
+	//printf("Han pasado 10 ms\n");
+}
+
+void *PC_keyboard (void *param) {
+	int teclaPulsada;
+
+	while (1) {
+
+		if (kbhit()) {
+			teclaPulsada = kbread();
+
+			switch (teclaPulsada) {
+				case 'l':
+					printf("\nLight button pressed!\n");
+					fflush(stdout);
+					button_light |= LIGHT_ACTIVE;
+					break;
+
+				case 'c':
+					printf("\nAlarm button pressed!\n");
+					fflush(stdout);
+					break;
+
+				case 'q':
+					printf("\nKey q pressed! Exiting...\n");
+					fflush(stdout);
+					exit(0);
+					break;
+
+				default:
+					printf("\nINVALID KEY\n");
+					fflush(stdout);
+					break;
+			}
+		}
+	}
+	
+}
+
 
 int main() {
+
+	printf("Starting program...\n");
+
+	printf("\nPlease press l to activate the light or c to introduce the code to arm/disarm the alarm system.\n");
+	printf("If you want to exit the program please press q.\n");
+
+	unsigned int next;
+
+	// Creates thread for keyboard lecture
+	pthread_t thread_keyboard_id;
+
+	pthread_create(&thread_keyboard_id, NULL, PC_keyboard, NULL);
+	// pthread_join(thread_keyboard_id, NULL);
 
 	fsm_trans_t light[] = {
 		{ LIGHT_OFF, LightButtonPressed, LIGHT_ON, ActivateLight },
@@ -41,13 +118,14 @@ int main() {
 
 	fsm_t* fsm_light = fsm_new (light);
 
-	next = millis();
-	while (true) {
+
+	next = clock()/CLOCKS_PER_SEC * 1000;
+	while (1) {
 		fsm_fire (fsm_light);
 
 		next += CLK_MS;
 		delay_until (next);
 	}
-	
+
 	return 0;
 }
--- 12/src/domotic.h
+++ 12/src/domotic.h
@@ -1,7 +1,24 @@
 #include "fsm.c"
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#include "kbhit.c" // Para poder detectar las teclas pulsadas sin bloqueo y leer las teclas pulsadas
 
 #define CLK_MS 10
+
 enum fsm_state {
 	LIGHT_ON,
 	LIGHT_OFF
 };
+
+#define LIGHT_ACTIVE	0x01
+
+void *PC_keyboard (void *param);
+
+int LightButtonPressed (fsm_t *this);
+int LightTimeoutReached (fsm_t *this);
+
+void ActivateLight (fsm_t *this);
+void DeactivateLight (fsm_t *this);
